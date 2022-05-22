fedf6a80d80691acc502583a11549bac09b4f86e
cihan <cihan.zereyalp@logsign.net>
2022-05-22 22:01:02 +0300

    test: create message

--- main_test.go
+++ main_test.go
@@ -1,1 +1,16 @@
 package main
+
+import "testing"
+
+func TestCreateMessage(t *testing.T) {
+	got := CreateMessage(Scrapper{
+		[]string{`https://webscraper.io/test-sites/e-commerce/allinone-popup-links`},
+		`yourWebHookURL`,
+		`<p>.*?\n.*?Welcome to (.*?) site\.`,
+	})
+	want := []string{`Players: WebScraper e-commerce`}
+
+	if got[0] != want[0] {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
